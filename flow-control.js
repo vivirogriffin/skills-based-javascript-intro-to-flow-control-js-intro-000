bfe0170994627fce24507c95c69bd981646c47a8
Vivien Griffin <vivirogriffin@gmail.com>
2021-03-06 22:01:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,19 @@
-function basicTeenager(age) {
-
+function basicTeenager(age) {if(age<= 19||age>=13)
+  console.log("You are a teenager!")
 }
 
-function teenager(age) {
-
+function teenager(age) {if(age<=19||age>=13)
+console.log ("You are a teenager!")
+} else {if(age>19||age<13)
+console.log ("You are not a teenager.")
 }
 
-function ageChecker(age) {
-
+function ageChecker(age) {if (age<=19||age>=13)
+console.log("You are a teenager!")
+} else if (age<13) {
+  console.log ("You are a mere child.")
+} else (age>19) {
+  console.log ("You are an adult")
 }
 
 function ternaryTeenager(age) {
